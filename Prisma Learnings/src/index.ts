23b3e044ca76a450c8a5170afa620480a486ab6a
Sidharth1327 <sidharthgnair1327@gmail.com>
2024-07-25 12:45:09 +0530

    Added Learnign s about Prisma, ORMs and how to perform CRUD operations using it

---
+++ Prisma Learnings/src/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function insertUser(username:string, password:string, firstName:string, lastName:string){
+  const res = await prisma.user.create({
+    data:{
+      email:username,
+      password,
+      firstName,
+      lastName
+    }
+  })
+  console.log(res);
+}
