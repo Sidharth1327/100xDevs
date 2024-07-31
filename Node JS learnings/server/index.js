84e355a6b9ff54d01e0657c374b8df00886e2837
Sidharth1327 <sidharthgnair1327@gmail.com>
2024-07-31 22:00:52 +0530

    Express and Node js, HTTP headers

--- Node JS learnings/server/index.js
+++ Node JS learnings/server/index.js
@@ -1,20 +1,17 @@
- const http = require("http");
- const fs = require("fs");
- const myServer = http.createServer((req,res)=>{
-  const log  = `${Date.now()}: ${req.url} New Req received\n`
-  fs.appendFile("log.txt", log,(err,data)=>{
-    switch(req.url){
-      case '/':
-        res.end("HomePage");
-        break;
-      case "/about":
-        res.end("I am Sid");
-        break;
-      default:
-        res.end("404 Not Found")    
-    }
-  })
-  res.end("Hello from server")
+
+ const express = require("express");
+
+ const app = express();
+
+ app.get('/', (req,res)=>{
+  return res.send(`Hello ${req.query.name}`);
+ })
+
+ app.get("/about", (req, res) => {
+   return res.send("Hello from about page");
  });
 
- myServer.listen(8000, ()=>console.log("server started"));
+ /* const myServer = http.createServer(app);
+
+ myServer.listen(8000, ()=>console.log("server started")); */
+ app.listen(8000, () => console.log("Server Started"));
